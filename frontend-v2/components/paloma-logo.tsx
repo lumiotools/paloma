b215707c1cf27f100757797929c7e57d471f7d76
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-05-05 20:25:25 +0530

    changed the width and height of the paloma logo

--- frontend-v2/components/paloma-logo.tsx
+++ frontend-v2/components/paloma-logo.tsx
@@ -10,19 +10,16 @@ export default function PalomaLogo({ className = "" }: { className?: string }) {
           alt="Paloma Realty Logo"
           width={200}
           height={200}
-          className="object-containw-[200px] h-auto mx-auto"
+          className="object-contain h-auto mx-auto w-[80px] xs:w-[100px] sm:w-[150px] md:w-[200px]"
           priority
           unoptimized
         />
       </div>
       <div className="text-center font-playfair mt-0">
-        <h2
-          className="font-normal tracking-wide leading-none"
-          style={{ fontSize: "48px" }}
-        >
+        <h2 className="font-normal tracking-wide leading-none text-[32px] xs:text-[40px] sm:text-[44px] md:text-[48px]">
           PALOMA
         </h2>
-        <h3 className="text-2xl tracking-widest -mt-1">REALTY</h3>
+        <h3 className="text-base sm:text-xl md:text-2xl tracking-widest -mt-1">REALTY</h3>
       </div>
     </div>
   );
